10d25b9a0ff178de0611a2875615cb0afc0ed4b2
jihyeon-songb <dhqtl1@naver.com>
2026-04-30 17:34:47 +0900

    feat: Add shadcn design system

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { TooltipProvider } from '@/components/ui/tooltip';
 import './globals.css';
 
 const geistSans = Geist({
@@ -24,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className='flex min-h-full flex-col'>{children}</body>
+      <body className='flex min-h-full flex-col'>
+        <TooltipProvider>{children}</TooltipProvider>
+      </body>
     </html>
   );
 }
